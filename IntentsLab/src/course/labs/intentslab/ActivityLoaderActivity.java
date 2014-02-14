e4b7bb5acc2b95dab655968514d835695a751d14
Alexander Quiros <seth29aqc@gmail.com>
2014-02-14 15:50:13 -0600

    Ready for testing, all guidelines implemented

--- IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
+++ IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
@@ -81,14 +81,15 @@ public class ActivityLoaderActivity extends Activity {
 
 		// TODO - Create a base intent for viewing a URL 
 		// (HINT:  second parameter uses parse() from the Uri class)
-		
+		Uri url = Uri.parse("http://www.google.com");
+		Intent urlIntent = new Intent(Intent.ACTION_VIEW, url);
 		
 		// TODO - Create a chooser intent, for choosing which Activity
 		// will carry out the baseIntent. Store the Intent in the 
 		// chooserIntent variable below. HINT: using the Intent class' 
 		// createChooser())
 		
-		Intent chooserIntent = null;
+		Intent chooserIntent =  Intent.createChooser(urlIntent, CHOOSER_TEXT);
 
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
 		// TODO - Start the chooser Activity, using the chooser intent
